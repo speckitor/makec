fddf341a99c39b604b4b9541a357f37928c5aa8b
speckitor <Speckitor@proton.me>
2026-07-02 22:03:12 +0700

    first commit

---
+++ README.md
@@ -0,0 +1,1 @@
+# Next generation build system for c
---
+++ build.c
@@ -0,0 +1,33 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+#include <sys/stat.h>
+
+void rebuild_build()
+{
+    char *cmd = "gcc build.c -o build";
+    printf("%s\n", cmd);
+    system(cmd);
+}
+
+void try_rebuild()
+{
+    struct stat exe_info;
+    struct stat src_info;
+
+    lstat("./build", &exe_info);
+    lstat("./build.c", &src_info);
+
+    if (exe_info.st_ctime < src_info.st_ctime)
+        rebuild_build();
+}
+
+int main(void)
+{
+    try_rebuild();
+
+    char *cmd = "gcc main.c";
+    printf("%s\n", cmd);
+    system(cmd);
+    return 0;
+}
